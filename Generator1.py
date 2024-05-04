c02031498e98535aa6d64ee0d2c5ee7657e23dbe
Purplepotatoe777 <168428249+Purplepotatoe777@users.noreply.github.com>
2024-05-04 16:55:10 +0200

    Update Generator1.py

--- Generator1.py
+++ Generator1.py
@@ -1,13 +1,14 @@
-from random import randint
 
+max_exponant = 4 # from 1 to 7 , the greater, the bigger & more difficult the deck. Set to {4,5,6,7} for level{1_1,1_2,1_3,1_4}
+min_exponant = 0
 deck=[]
 
-for i in range(8) :
+for i in range(min_exponant, max_exponant) :
   for j in range (i-1):
-    deck[i][0] = stg(2**i) + '+'+  stg(2**j)
+    deck[i][0] = stg(2**i) + '+'+  stg(2**j) 
     deck[i][1] = stg( 2**i + 2**j)
 
-print(deck)
+print(deck) # should be store in txt file
   
 
    
