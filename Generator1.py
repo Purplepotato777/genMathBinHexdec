3100c27ca88ee615abc67dadd0549dfb998a9412
Purplepotatoe777 <168428249+Purplepotatoe777@users.noreply.github.com>
2024-05-05 09:43:19 +0200

    Update Generator1.py
    fonctionnal todo export>txt

--- Generator1.py
+++ Generator1.py
@@ -1,12 +1,13 @@
 
-max_exponant = 4 # from 1 to 7 , the greater, the bigger & more difficult the deck. Set to {4,5,6,7} for level{1_1,1_2,1_3,1_4}
-min_exponant = 0
+max_exponant = 7 # from 1 to 7 , the greater, the bigger & more difficult the deck. Set to {4,5,6,7} for level{1_1,1_2,1_3,1_4}
 deck=[]
 
-for i in range(min_exponant, max_exponant) :
-  for j in range (i-1):
-    deck[i][0] = stg(2**i) + '+'+  stg(2**j) 
-    deck[i][1] = stg( 2**i + 2**j)
+for i in range(1, max_exponant+1) :
+ 
+  for j in range (i+1):
+    a = str(2**i) + '+'+  str(2**j) 
+    b = str(2**i + 2**j)
+    deck.append([a,b])
 
 print(deck) # should be store in txt file
   
